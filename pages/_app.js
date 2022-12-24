acc876a77534d65dda4a6e819453e39e1fd12a16
Rahul Kumar Singh <rsingh@pcs.com.np>
2022-12-24 22:12:02 +0545

    Initial Setup, Removed unused files

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "@mui/material";
+import "../styles/globals.scss";
+import theme from "../styles/theme";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
